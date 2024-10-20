66e7bf602f1b7cb45fcb9c957d1f2514857e9d19
Adarsh Vardhan <146087866+adarshraj5206@users.noreply.github.com>
2024-10-20 02:20:18 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using node, not browser.
+
+console.log(3
+    +
+    3); // code readability should be high
+
+console.log("Adasrh");
+
+let name =  "adarsh" // string data type
+let age = 18 // integer data type
+let isLoggedIn = true // boolean data type
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// Boolean => true/false
+// null  =>  it is a standalone value but also is a data type
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "harsh vardhan");
+console.log(typeof age);
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
+
