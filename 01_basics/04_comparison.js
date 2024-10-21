7dd99fa978c4bcf9aa9de703625a6c23a699d9be
Adarsh Vardhan <146087866+adarshraj5206@users.noreply.github.com>
2024-10-21 12:34:28 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log( 2 > 1);    //true
+// console.log( 2 >= 1 );  //true
+// console.log( 2 < 1);    //false
+// console.log( 2 <= 1 );  //false
+// console.log( 2 == 1);   //false
+// console.log( 2 != 1 );  //true
+
+// console.log( "2" > 1); // true
+// console.log( "02" > 1); // true
+
+// these conversions can confuse us at any time so we avoid these comparisons most of the time 
+
+// console.log( null > 0);   //false
+// console.log( null == 0);  //false
+// console.log( null >= 0);  //true
+
+// console.log( undefined == 0);    //false
+// console.log( undefined > 0 );  //false
+// console.log(undefined < 0)  //false
+
+
+// === strict check
+
+console.log("2"===1) // false
