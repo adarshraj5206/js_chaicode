20a9199cb3396939dbe6eedd4357e978c1c56c79
Adarsh Vardhan <146087866+adarshraj5206@users.noreply.github.com>
2024-10-21 09:19:27 +0000

    conversion-operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33ab"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+//  "33ab" => NaN
+// true => 1; false =>0;
+
+let isLoggedIn = 1;
+
+let booleanISLoggedIn = Boolean(isLoggedIn);
+console.log(booleanISLoggedIn); //true
+
+// 1 => true
+// 0 => false
+// "" => false
+// "any_string" => true
+
+let SomeNumber = 33;
+
+let StringNumber = String(SomeNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
