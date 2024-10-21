039c485aa43fca1745b9df7f053d3d72f1019ce3
Adarsh Vardhan <146087866+adarshraj5206@users.noreply.github.com>
2024-10-21 12:58:18 +0000

    datatype-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,21 @@
+// primitive
+
+// 7 types :-  string, Number, Boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+// both id and anotherId's symbols have the same description, they are unique and not equal to each other.
+
+console.log(id === anotherId); // false
+
+
+// Non Primitive ( Non Primitive)
+
+// Array, objects, functions
