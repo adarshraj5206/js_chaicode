5f921cd219c4419c8a5011a559426fd5e6de02e9
Adarsh vardhan <adarshraj5206@gmail.com>
2024-11-02 11:38:44 +0530

    funtions with objects

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -25,7 +25,7 @@ function addTwoNumber(num1, num2){
 }
 
 const result = addTwoNumber(3,5);
-console.log("Result : ",result);
+// console.log("Result : ",result);
 
 
 function loginUserMessage(username){
@@ -33,4 +33,38 @@ function loginUserMessage(username){
 }
 
 // console.log(loginUserMessage("shani"));
-console.log(loginUserMessage()); // undefined
+// console.log(loginUserMessage()); // undefined
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1;
+}
+
+// console.log(calculateCartPrice(200,3000,405,201))
+
+
+const user = {
+    username : "Adarsh",
+    price : 999
+}
+
+function handelobj( anyobject ){
+    console.log(`user name is ${ anyobject.username } and price is ${anyobject.price }`);
+    
+}
+
+// handelobj(user)
+handelobj({
+    username : "Adarsh",
+    price : 999
+})
+
+
+
+const myNewArray = [ 200,520, 360,410 ]
+
+function returnSecondValue(getArray){
+    return getArray[2]
+}
+
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([ 200,520, 360,410 ]));
