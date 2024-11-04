7dd4a9e40feecbb42d040a0519a4bf4c243f2aee
Adarsh vardhan <adarshraj5206@gmail.com>
2024-11-04 05:48:58 +0530

    hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,18 +6,18 @@ function showGlobalVar() {
     console.log(globalVar); // Accessible here
 }
 
-showGlobalVar(); // Outputs: "I'm global!"
+// showGlobalVar(); // Outputs: "I'm global!"
 
 // Local scope / Block scope ->>> Local scope in JavaScript refers to variables that are declared within a function or block. These variables are only accessible within that specific function or block, preventing them from interfering with other parts of your code
 
 function greet() {
     var localVar = "Hello!";
-    console.log(localVar); // Accessible here
+    // console.log(localVar); // Accessible here
 }
 
 greet(); // Outputs: "Hello!"
 
-console.log(localVar); // Error: localVar is not defined
+// console.log(localVar); // Error: localVar is not defined
 
 
 // Note : ---> 
@@ -27,3 +27,47 @@ console.log(localVar); // Error: localVar is not defined
     // var is function-scoped, meaning it can be accessed anywhere within the function, even before it is declared.
 
     // const and let are not mutable, meaning their values cannot be changed once they are assigned.
+
+function one(){
+    const userName = "Adarsh"
+
+    function two(){
+        const website = "Youtube"
+        console.log(userName);
+    }
+    // console.log(website);
+
+    two();
+}
+
+// one();
+
+
+if (true){
+    const userName = "Aman"
+    if( userName === "Aman"){
+        const website = " instagram"
+        // console.log(userName + website);
+    }
+    // console.log(website); // error
+}
+
+// console.log(userName); //error
+
+
+
+
+// **************** Hoisting ****************
+
+console.log( addone(5));
+function addone(num){
+    return num + 1;
+}
+
+
+addTwo(5) // error bcz addTwo hold fuction but it treated as a variable
+
+const addTwo = function(num){
+    return num + 2
+}
+
