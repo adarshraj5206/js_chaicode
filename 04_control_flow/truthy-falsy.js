c1677686cc504a5c7ff30c90f1f5b5d2e5ae61c7
Adarsh vardhan <adarshraj5206@gmail.com>
2024-11-06 21:59:30 +0530

    truthy falsy values

--- 04_control_flow/truthy-falsy.js
+++ 04_control_flow/truthy-falsy.js
@@ -21,3 +21,20 @@ if (emptyArray.length === 0) {
     console.log("truthy"); 
 }
 
+// Nullish Coalescing Operator (??): null, undefined
+
+let val1;
+// val1 = 5 ?? 10; // 5
+// val1 = null ?? 10; // 10
+// val1 = undefined ?? 15; // 15
+
+val1 = null ?? 10 ?? 20; // 10
+console.log(val1);
+
+
+// Ternary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100;
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");  
