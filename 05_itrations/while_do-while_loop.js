6578f7343e1064a3268691719fda32c7eb81a1ac
Adarsh vardhan <adarshraj5206@gmail.com>
2024-11-07 14:15:18 +0530

    while  do-while

---
+++ 05_itrations/while_do-while_loop.js
@@ -0,0 +1,26 @@
+// while loop
+
+// let i = 0;
+// while (i < 10) {
+//     console.log(i);
+//     i++;
+// }
+
+// array in while loop
+
+let myArray = ["flash", "batman", "superman"];
+let arr = 0;
+while (arr < myArray.length) {
+    // console.log(myArray[i]);
+    arr++;
+}
+
+// do while loop 
+
+let score = 11;
+do {
+    console.log(score);
+    score++;
+} while (score < 10);
+
+
