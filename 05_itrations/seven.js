90c70ae6eb268bff007fd0f295574b18980afcae
Adarsh vardhan <adarshraj5206@gmail.com>
2024-11-08 22:48:41 +0530

    seven

---
+++ 05_itrations/seven.js
@@ -0,0 +1,22 @@
+//  const myNums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+//  const newNums = myNums.map( (num) => num + 10 )
+//  console.log(newNums);
+
+
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const newNums = [];
+
+// myNums.forEach((num) => {
+//   newNums.push(num + 10);
+// });
+
+// console.log(newNums);
+
+
+const myNums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+
+const newNums = myNums
+                .map( (num) => num * 10 ) // [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+                .map( (num) => num + 1 ) // [11, 21, 31, 41, 51, 61, 71, 81, 91, 101]
+                .filter( (num) => num >= 40 ) // [41, 51, 61, 71, 81, 91, 101]
+console.log(newNums);
